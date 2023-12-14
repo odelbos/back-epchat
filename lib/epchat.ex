64ee30b8c6d69068d74fc4a65938a17c5680a718
odelbos <od@phibox.com>
2023-12-14 15:05:14 +0100

    initial commit

---
+++ lib/epchat.ex
@@ -0,0 +1,18 @@
+defmodule Epchat do
+  @moduledoc """
+  Documentation for `Epchat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Epchat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/epchat_test.exs
@@ -0,0 +1,8 @@
+defmodule EpchatTest do
+  use ExUnit.Case
+  doctest Epchat
+
+  test "greets the world" do
+    assert Epchat.hello() == :world
+  end
+end
