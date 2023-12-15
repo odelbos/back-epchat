7d53b0d85749438c4cf64b41b3d896a5c45580f9
odelbos <od@phibox.com>
2023-12-15 17:22:56 +0100

    add schema for 'memberships' table

--- lib/epchat/db/schema.ex
+++ lib/epchat/db/schema.ex
@@ -1,9 +1,11 @@
 defmodule Epchat.Db.Schema do
   require Logger
+  alias Epchat.Db.Db
 
   def init(conn) do
     create_users_table conn
     create_channels_table conn
+    create_memberships_table conn
   end
 
   def create_users_table(conn) do
@@ -13,7 +15,7 @@ defmodule Epchat.Db.Schema do
           nickname TEXT NOT NULL
       );
     """
-    case Epchat.Db.Db.query conn, query do
+    case Db.query conn, query do
       {:error, reason} ->
         Logger.debug "Cannot create 'users' table, reason: #{reason}"
         raise "Cannot create 'users' table"
@@ -30,10 +32,30 @@ defmodule Epchat.Db.Schema do
           FOREIGN KEY (owner_id) REFERENCES users (id)
       );
     """
-    case Epchat.Db.Db.query conn, query do
+    case Db.query conn, query do
       {:error, reason} ->
         Logger.debug "Cannot create 'channels' table, reason: #{reason}"
         raise "Cannot create 'channels' table"
+      _ -> :ok
+    end
+  end
+
+  defp create_memberships_table(conn) do
+    query = """
+      CREATE TABLE IF NOT EXISTS memberships (
+          channel_id text TEXT,
+          user_id TEXT,
+          pid TEXT NOT NULL,
+          joined_at INTEGER NOT NULL,
+          PRIMARY KEY (channel_id, user_id),
+          FOREIGN KEY (channel_id) REFERENCES channels (id),
+          FOREIGN KEY (user_id) REFERENCES users (id)
+      );
+    """
+    case Db.query conn, query do
+      {:error, reason} ->
+        Logger.debug "Cannot create 'memberships' table, reason: #{reason}"
+        raise "Cannot create 'memberships' table"
       _ -> :ok
     end
   end
