c73c73ff7333dd265a0fd756ad74b1b38197c180
odelbos <od@phibox.com>
2023-12-15 11:10:12 +0100

    add 'channels' table creation in schema

--- lib/epchat/db/schema.ex
+++ lib/epchat/db/schema.ex
@@ -3,6 +3,7 @@ defmodule Epchat.Db.Schema do
 
   def init(conn) do
     create_users_table conn
+    create_channels_table conn
   end
 
   def create_users_table(conn) do
@@ -16,6 +17,23 @@ defmodule Epchat.Db.Schema do
       {:error, reason} ->
         Logger.debug "Cannot create 'users' table, reason: #{reason}"
         raise "Cannot create 'users' table"
+      _ -> :ok
+    end
+  end
+
+  def create_channels_table(conn) do
+    query = """
+      CREATE TABLE IF NOT EXISTS channels (
+          id text PRIMARY KEY,
+          owner_id TEXT NOT NULL,
+          last_activity_at INTEGER NOT NULL,
+          FOREIGN KEY (owner_id) REFERENCES users (id)
+      );
+    """
+    case Epchat.Db.Db.query conn, query do
+      {:error, reason} ->
+        Logger.debug "Cannot create 'channels' table, reason: #{reason}"
+        raise "Cannot create 'channels' table"
       _ -> :ok
     end
   end
