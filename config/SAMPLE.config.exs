82f9c1a94f7298193ef5b260afe68eeea6b9c696
odelbos <od@phibox.com>
2024-01-09 14:26:30 +0100

    add members_limit to config

--- config/SAMPLE.config.exs
+++ config/SAMPLE.config.exs
@@ -9,6 +9,7 @@ config :epchat,
     ids_length: 15,
   },
   channels: %{
+    members_limit: 10,
     token_life_time: 300,         # 5mn in seconds
     inactivity_interval: 300,     # 5mn In seconds
     inactivity_limit: 600         # 10mn in sseconds
